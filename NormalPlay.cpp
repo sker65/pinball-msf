f59fa6b9fd8cfdef8ad9f5e34740f795cda8c28a
Stefan Rinke <sker65@gmail.com>
2021-12-03 21:30:20 +0100

    - added format - remove unused code

--- NormalPlay.cpp
+++ NormalPlay.cpp
@@ -10,13 +10,12 @@
 
 // HSV Rainbow
 extern const TProgmemRGBPalette16 normalPallette PROGMEM = {
-		0xFFD690, 0xFFD690, 0xFFD690, 0x0000FF,
-		0xFFD6A0, 0xFFD6A0, 0xFF0000, 0xFF0000,
-		0x0000FF, 0x0000FF, 0x0000FF, 0xFFD690,
-		0xFFD690, 0x0000FF, 0x0000FF, 0xFFD690, };
+    0xFFD690, 0xFFD690, 0xFFD690, 0x0000FF, 0xFFD6A0, 0xFFD6A0, 0xFF0000, 0xFF0000,
+    0x0000FF, 0x0000FF, 0x0000FF, 0xFFD690, 0xFFD690, 0x0000FF, 0x0000FF, 0xFFD690,
+};
 
-NormalPlay::NormalPlay(const int* index, int num, int animationSpeed, int holdCycles, int pin) :
-		Effect(0, index, num) {
+NormalPlay::NormalPlay( const int* index, int num, int animationSpeed, int holdCycles, int pin )
+    : Effect( 0, index, num ) {
 	paletteIndex = 0;
 	nextUpdate = 0;
 	this->animationDelay = 1000 / animationSpeed;
@@ -25,22 +24,22 @@ NormalPlay::NormalPlay(const int* index, int num, int animationSpeed, int holdCy
 	this->giSense = pin;
 }
 
-CRGB warmWhite100( 255, 214, 170); // 0xFF, 0xD6, 0xAA
+CRGB warmWhite100( 255, 214, 170 ); // 0xFF, 0xD6, 0xAA
 
-bool NormalPlay::updateLeds(unsigned long now, CRGB* leds) {
-	if (active) {
-//		if( digitalRead(this->giSense) == LOW ) {
-//			CRGB black(0, 0, 0);
-//			for (int i = 0; i < num; i++) {
-//				leds[index[i]] = black;
-//			}
-//		} else
-		if( now > nextUpdate) {
+bool NormalPlay::updateLeds( unsigned long now, CRGB* leds ) {
+	if( active ) {
+		//		if( digitalRead(this->giSense) == LOW ) {
+		//			CRGB black(0, 0, 0);
+		//			for (int i = 0; i < num; i++) {
+		//				leds[index[i]] = black;
+		//			}
+		//		} else
+		if( now > nextUpdate ) {
 			nextUpdate = now + animationDelay;
 
 			// add a hold at 0, 16, 32
-			if(( paletteIndex % 16)== 0 ) {
-				if( cycle++ > holdCycles) {
+			if( ( paletteIndex % 16 ) == 0 ) {
+				if( cycle++ > holdCycles ) {
 					paletteIndex = paletteIndex + 1;
 					cycle = 0;
 				}
@@ -50,8 +49,8 @@ bool NormalPlay::updateLeds(unsigned long now, CRGB* leds) {
 			int colorIndex = paletteIndex;
 			CRGBPalette16 pal = normalPallette;
 
-			for (int i = 0; i < num; i++) {
-				leds[index[i]] = ColorFromPalette(pal, colorIndex, 255, LINEARBLEND);
+			for( int i = 0; i < num; i++ ) {
+				leds[index[i]] = ColorFromPalette( pal, colorIndex, 255, LINEARBLEND );
 			}
 			return true;
 		}
--- pinball-msf.ino
+++ pinball-msf.ino
@@ -1,8 +1,6 @@
 #include "Flash.h"
-#include "Attract.h"
-#include "NormalPlay.h"
-#include "MultiBallPlay.h"
 #include "LedDriver.h"
+#include "NormalPlay.h"
 #include "SlingFlash.h"
 #include "SpeakerFlash.h"
 #include <FastLED.h>
@@ -10,50 +8,42 @@
 // for MSF
 // 48-77 top bar from left to right
 
-int slingRightLeds[] = {107, 108};
-int slingLeftLeds[] = {103, 104};
+int slingRightLeds[] = { 107, 108 };
+int slingLeftLeds[] = { 103, 104 };
 
-CRGB white(255, 255, 255);
-//CRGB warmWhite100( 255, 214, 170);
+CRGB white( 255, 255, 255 );
+// CRGB warmWhite100( 255, 214, 170);
 
-Flash slingRightFlash(500, &white, slingRightLeds, 2, 1);
-Flash slingLeftFlash(500, &white, slingLeftLeds, 2, 1);
+Flash slingRightFlash( 500, &white, slingRightLeds, 2, 1 );
+Flash slingLeftFlash( 500, &white, slingLeftLeds, 2, 1 );
 
-SlingFlash slingRight(&slingRightFlash);
-SlingFlash slingLeft(&slingLeftFlash);
+SlingFlash slingRight( &slingRightFlash );
+SlingFlash slingLeft( &slingLeftFlash );
 
 const int allLeds[] = { // von unten nach oben
-		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-		17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33,
-		34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50,
-		51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
-		68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80,
-		81, 82, 83, 84, 85, 86, 87, 88, 89, 90,
-		91, 92, 93, 94, 95, 96, 97, 98, 99, 100,
-		101, 102, 103, 104, 105, 106, 107, 198, 109, 110,
-		111, 112, 113, 114, 115};
+    0,  1,  2,  3,  4,   5,   6,   7,   8,   9,   10,  11,  12,  13,  14,  15,  16,  17,  18,  19, 20, 21, 22, 23,
+    24, 25, 26, 27, 28,  29,  30,  31,  32,  33,  34,  35,  36,  37,  38,  39,  40,  41,  42,  43, 44, 45, 46, 47,
+    48, 49, 50, 51, 52,  53,  54,  55,  56,  57,  58,  59,  60,  61,  62,  63,  64,  65,  66,  67, 68, 69, 70, 71,
+    72, 73, 74, 75, 76,  77,  78,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91, 92, 93, 94, 95,
+    96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 198, 109, 110, 111, 112, 113, 114, 115 };
 
 // 0-23 right speaker
-const int rightSpeaker[] = {
-		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
+const int rightSpeaker[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
 
 // 24-47 left speaker
-const int leftSpeaker[] = {
-		24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47};
+const int leftSpeaker[] = { 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+                            36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47 };
 
 // 48-77 top bar from left to right
-const int topBar[] = {
-		48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77};
+const int topBar[] = { 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62,
+                       63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77 };
 
 // 78 - 79 creature
-const int creature[] = {
-		78, 79};
+const int creature[] = { 78, 79 };
 // 92 - 93 upper scoop
-const int upperScoop[] = {
-		92, 93};
+const int upperScoop[] = { 92, 93 };
 // 99 - 100 lower scoop
-const int lowerScoop[] = {
-		99, 100};
+const int lowerScoop[] = { 99, 100 };
 // 80 - 91 GI upper
 // 94 - 98 GI left
 // 101 - 102 GI
@@ -62,49 +52,32 @@ const int lowerScoop[] = {
 // 107 - 108 right sling
 // 109 - 115 GI right
 
-const int playfieldGi[] = {
-		80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91,
-		94, 95, 96, 97, 98,
-		101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115};
+const int playfieldGi[] = { 80, 81,  82,  83,  84,  85,  86,  87,  88,  89,  90,  91,  94,  95,  96,  97,
+                            98, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115 };
 
 #define GI_SENSE_PIN 8
 
-//Attract attract(allLeds, ALL_LEDS, 20);  // array of led index, num, updates per sec
+NormalPlay normalPlay( playfieldGi, sizeof( playfieldGi ) / sizeof( int ), 15, 60, GI_SENSE_PIN );
+SpeakerFlash speakerFlash( leftSpeaker, sizeof( leftSpeaker ) / sizeof( int ), rightSpeaker,
+                           sizeof( rightSpeaker ) / sizeof( int ), 50, 2 );
 
-NormalPlay normalPlay(playfieldGi, sizeof(playfieldGi) / sizeof(int), 15, 60, GI_SENSE_PIN);
-SpeakerFlash speakerFlash(leftSpeaker, sizeof(leftSpeaker) / sizeof(int),
-													rightSpeaker, sizeof(rightSpeaker) / sizeof(int), 50, 2);
+CRGB creatureColor( 0, 255, 0 );
 
-//MultiBallPlay multiBallPlay(allLeds, ALL_LEDS, 20);
-CRGB creatureColor(0, 255, 0);
+Flash creatureFlash( 50, &creatureColor, creature, sizeof( creature ) / sizeof( int ), 1 );
 
-Flash creatureFlash(50, &creatureColor, creature, sizeof(creature) / sizeof(int), 1);
+LedDriver ledDriver( 40 );
 
-LedDriver ledDriver(40);
-
-// synchronization between interrupt routines and FASTLED.show() needed
-// switch callback also on a regular basis, not only edge
-// maybe do the FASTLED.show() always at the end of a scan cycle when Timer1 stops
-
-void setup()
-{
-	//DDRA = 0; // all PORTA is input
-	Serial.begin(9600);
+void setup() {
+	// DDRA = 0; // all PORTA is input
+	Serial.begin( 9600 );
 
 	ledDriver.begin();
 
 	// pinMode(GI_SENSE_PIN, INPUT_PULLUP);
 
-	//	ledDriver.registerEffect(&attract);
-	ledDriver.registerEffect(&normalPlay);
-	ledDriver.registerEffect(&speakerFlash);
-	ledDriver.registerEffect(&creatureFlash);
-	//ledDriver.registerEffect(&multiBallPlay);
-	//ledDriver.registerEffect(&slingLeftFlash);
-	//ledDriver.registerEffect(&slingRightFlash);
-
-	//switchScanner.registerSwitchAction(4,4,&slingRight, EDGE);
-	//switchScanner.registerSwitchAction(3,4,&slingLeft, EDGE);
+	ledDriver.registerEffect( &normalPlay );
+	ledDriver.registerEffect( &speakerFlash );
+	ledDriver.registerEffect( &creatureFlash );
 
 	// this is active by default
 	normalPlay.active = true;
@@ -113,17 +86,14 @@ void setup()
 
 uint32_t nextCheck = 0;
 
-void loop()
-{
+void loop() {
 	uint32_t now = millis();
-	//switchScanner.update(now);
-	ledDriver.update(now);
-	if (now > nextCheck)
-	{
+	// switchScanner.update(now);
+	ledDriver.update( now );
+	if( now > nextCheck ) {
 		nextCheck = now + 300;
-		int r = random(10);
-		if (r == 0)
-		{
+		int r = random( 10 );
+		if( r == 0 ) {
 			creatureFlash.start();
 		}
 	}
